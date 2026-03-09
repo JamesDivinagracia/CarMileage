8ddd21759a74223f537d4d750d93359ae49f6244
Filip Lenczkiewicz <flenczkiewicz@gmail.com>
2026-03-09 17:23:35 +0000

    Add files via upload

---
+++ CA02/ca2.java
@@ -0,0 +1,9 @@
+//import java.time. LocalDate;
+//import java.time.format.Datetimeformatter;
+//import java.util.Locale;
+
+public class ( ReadDate
+
+	public static void main (String[] args) {
+		DateTimeFormatter = DateTimeFormatter.ofPattern(
+					"dd/MM/
