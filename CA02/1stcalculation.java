d7cd8d82334ffdfeb29ae5f1a9a88e3dfa8bbdb4
Filip Lenczkiewicz <flenczkiewicz@gmail.com>
2026-03-09 19:08:40 +0000

    Add Mileage class to calculate fuel efficiency

--- CA02/1stcalculation.java
+++ CA02/1stcalculation.java
@@ -1,1 +1,23 @@
 
+public class Mileage {
+	
+	public static float calculateEfficiency(float litres, int prevkm, int currentkm) {
+		int distance = currentkm - prevkm;
+		return (litres / distance) * 100;
+	}
+	
+	public static void main(String[] args) {
+		
+		//mileage 
+		int[] km = {24886, 25319, 25740, 26141};
+		
+		//litres at each fill 
+		float[] litres = {27.59, 29.55 , 28.98 , 27.13};
+		
+		
+	// start at 1 becasue each calculation of efficiency needs previous reading 
+	for ( int i = 1; i < km.length; i++) {
+		
+		float efficiency = calculateEfficiency(litres[i], km [i-1], km[i]);
+		
+		system.out.printf("%.2f L/100km%n" , efficiency);
