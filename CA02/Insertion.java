9ee94358cf447ac382f6bf4a391ae7951e920023
Floppius <d.jmarte02@gmail.com>
2026-03-15 23:00:02 +0000

    final changes

--- CA02/Insertion.java
+++ CA02/Insertion.java
@@ -1,26 +1,56 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-import java.lang.Math;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+/******************************************************************************
+ *  Compilation:  javac Insertion.java
+ *  Execution:    java Insertion < input.txt
+ *  Dependencies: StdOut.java StdIn.java
+ *  Data files:   https://algs4.cs.princeton.edu/21elementary/tiny.txt
+ *                https://algs4.cs.princeton.edu/21elementary/words3.txt
+ *
+ *  Sorts a sequence of strings from standard input using insertion sort.
+ *
+ *  % more tiny.txt
+ *  S O R T E X A M P L E
+ *
+ *  % java Insertion < tiny.txt
+ *  A E E L M O P R S T X                 [ one string per line ]
+ *
+ *  % more words3.txt
+ *  bed bug dad yes zoo ... all bad yet
+ *
+ *  % java Insertion < words3.txt
+ *  all bad bed bug dad ... yes yet zoo   [ one string per line ]
+ *
+ ******************************************************************************/
+
 import java.util.Comparator;
+import java.util.Scanner;
+import java.util.*;
+/**
+ *  The {@code Insertion} class provides static methods for sorting an
+ *  array using insertion sort.
+ *  <p>
+ *  In the worst case, this implementation makes ~ &frac12; <em>n</em><sup>2</sup>
+ *  compares and ~ &frac12; <em>n</em><sup>2</sup> exchanges to sort an array
+ *  of length <em>n</em>. So, it is not suitable for sorting large arbitrary
+ *  arrays. More precisely, the number of exchanges is exactly equal to the
+ *  number of inversions. So, for example, it sorts a partially-sorted array
+ *  in linear time.
+ *  <p>
+ *  This sorting algorithm is stable.
+ *  It uses &Theta;(1) extra memory (not including the input array).
+ *  <p>
+ *  See <a href="https://algs4.cs.princeton.edu/21elementary/InsertionPedantic.java.html">InsertionPedantic.java</a>
+ *  for a version that eliminates the compiler warning.
+ *  <p>
+ *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/21elementary">Section 2.1</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ */
+public class Insertion {
 
-public class CA02 {
-
-	// Insertion sort
-		/*
-			* James: Any sorting algorithm works, we will just explain the time complexity and 
-	 		* 			reason as to why this sorting algorithm will suffice.
- 			*
- 			* Source: https://algs4.cs.princeton.edu/21elementary/Insertion.java.html 	
-			* 				(Modified for this CA)
-			*
-			*	Insertion class name is changed to CA02
-			*
-		*/
- // This class should not be instantiated.
-    private CA02() { }
+    // This class should not be instantiated.
+    private Insertion() { }
 
     /**
      * Rearranges the array in ascending order, using the natural order.
@@ -172,16 +202,10 @@ public class CA02 {
      *
      * @param args the command-line arguments
      */
-    
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         String[] a = StdIn.readAllStrings();
-        CA02.sort(a);
+        Insertion.sort(a);
         show(a);
-   	
-		//eff calc
-		//min max main
-		
-	 }
-
-
+    }
 }
+
--- CA02/ca2.java
+++
@@ -1,9 +0,0 @@
-//import java.time. LocalDate;
-//import java.time.format.Datetimeformatter;
-//import java.util.Locale;
-
-public class ( ReadDate
-
-	public static void main (String[] args) {
-		DateTimeFormatter = DateTimeFormatter.ofPattern(
-					"dd/MM/
